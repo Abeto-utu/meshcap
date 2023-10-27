d9a9a7ebdde3308b4760b27d4bf579778a853c0f
Amaral-Genaro <genaro.amaral100@gmail.com>
2023-10-27 18:31:11 -0300

    HOMEPAGE pasado a boostrap

--- src/HOMEPAGE/CONTROLADOR/consultaEstadoPaquete.php
+++ src/HOMEPAGE/CONTROLADOR/consultaEstadoPaquete.php
@@ -5,16 +5,25 @@ session_start();
 if(isset($_POST['numero_paquete']) && isset($_POST['rastreo'])) {
     $numero = $_POST['numero_paquete'];
 
-    $query = "SELECT * FROM paquete WHERE id_paquete = $numero";
+    $query = "SELECT pa.id_paquete, pa.estado, vp.id_lote, v.matricula, u.nombre
+    FROM paquete_lote p
+    JOIN vehiculo_plataforma_lote vp ON p.id_lote = vp.id_lote
+    JOIN paquete pa ON p.id_paquete = pa.id_paquete
+    JOIN plataforma_lote pl ON vp.id_lote = pl.id_lote
+    JOIN vehiculo v ON vp.matricula = v.matricula
+    JOIN camionero_vehiculo cv ON v.matricula = cv.matricula
+    JOIN usuario u ON cv.id_usuario = u.id_usuario
+    WHERE p.id_paquete = $numero;
+    ";
     $paqueteSeleccionado = mysqli_query($conn, $query);
 
     if(!$paqueteSeleccionado) {
-        $_SESSION['no_encontrado'] = true;
-    }
+        header("Location: ../VISTA/index.php?error=pne#paquete");
+    } else 
 
     $_SESSION['resultado_paquete'] = mysqli_fetch_assoc($paqueteSeleccionado);
 
-    header("Location: ../VISTA/rastrearPaquete.php");
+    header("Location: ../VISTA/index.php#paquete");
     exit;
 }
 ?>
--- src/HOMEPAGE/VISTA/index.php
+++ src/HOMEPAGE/VISTA/index.php
@@ -15,14 +15,11 @@
             scroll-behavior: smooth;
         }
 
-        .navbar,
-        .faq {
+        .navbar {
             padding: 10px 17.5%;
         }
 
         @media only screen and (max-width: 600px) {
-
-            .faq,
             .navbar {
                 padding: 10px 3%;
             }
@@ -51,7 +48,7 @@
 <body class="d-flex flex-column min-vh-100 bg-dark">
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="index.php#inicio" data-i18n="quickCarry">Quick Carry</a>
+            <a class="navbar-brand" href="#inicio" data-i18n="quickCarry">Quick Carry</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -66,10 +63,10 @@
                             language</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php#paquete" data-i18n="trackPackage">Rastrear Paquete</a>
+                        <a class="nav-link" href="#paquete" data-i18n="trackPackage">Rastrear Paquete</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php#contacto" data-i18n="contact">Contacto</a>
+                        <a class="nav-link" href="#contacto" data-i18n="contact">Contacto</a>
                     </li>
                 </ul>
             </div>
@@ -78,36 +75,59 @@
 
     <section id="inicio">
         <div class="container bg-dark text-white text-center py-5">
-            <h1 class="display-4" data-i18n="loginTitle">Iniciar sesión</h1>
-            <p class="text-white-50 mb-5" data-i18n="loginSubtitle">Por favor ingrese sus credenciales</p>
-            <form action="../CONTROLADOR/recibirDatos.php" method="POST">
+            <h1 class="display-4" data-i18n="quickCarry">Quick Carry</h1>
+            <h2 data-i18n="reliableLogistics">La empresa de logística más confiable del Uruguay</h2>
+            <div class="mt-4">
+                <a href="#paquete" class="btn btn-warning btn-lg" data-i18n="trackPackage">Rastrear paquete</a>
+            </div>
+        </div>
+    </section>
+    <section id="paquete">
+        <div class="container bg-dark text-white text-center py-5">
+            <h2 class="display-4" data-i18n="trackPackage">Rastrear paquete</h2>
+            <form action="../CONTROLADOR/consultaEstadoPaquete.php" method="POST">
                 <div class="form-row align-items-center">
                     <div class="col-auto" style="display: flex;align-items: center; justify-content: center;">
-                        <div class="form-outline form-white mb-4">
-                            <label class="form-label" for="typeEmailX" data-i18n="name">N° Funcionario</label>
-                            <input type="number" id="typeEmailX" class="form-control form-control-lg"
-                                name="numero_funcionario" />
-                        </div>
+                        <label for="numberInput" class="sr-only" data-i18n="enterIdentifier">Ingrese el
+                            identificador:</label>
+                        <input class="form-control mb-2" id="numberInput" name="numero_paquete"
+                            placeholder="Ingrese el identificador" name="numberInput" style="max-width: 300px;">
                     </div>
-                    <div class="col-auto" style="display: flex;align-items: center; justify-content: center;">
-                        <div class="form-outline form-white mb-4">
-                            <label class="form-label" for="typePasswordX" data-i18n="email">Contraseña</label>
-                            <input type="password" id="typePasswordX" class="form-control form-control-lg" name="clave"
-                                maxlength="16" minlength="6" />
-                        </div>
-                    </div>
-
-                    <button class="btn btn-outline-light btn-lg px-5" type="submit" data-i18n="send">Ingresar</button>
-
-                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
-                        <a href="https://facebook.com" class="text-white"><i
-                                class="fab fa-facebook-f fa-lg m-3"></i></a>
-                        <a href="https://twitter.com" class="text-white"><i class="fab fa-twitter fa-lg m-3"></i></a>
-                        <a href="https://www.google.com/intl/es/gmail/about" class="text-white"><i
-                                class="fab fa-google fa-lg m-3"></i></a>
+                    <div class="col-auto">
+                        <button type="submit" value="Rastrear" name="rastreo" class="btn btn-warning mb-2"
+                            data-i18n="track">Rastrear</button>
                     </div>
                 </div>
             </form>
+            <hr>
+            <?php
+            if (isset($_SESSION['resultado_paquete'])) {
+                $resultado_paquete = $_SESSION['resultado_paquete'];
+
+                ?>
+                <p class="p-2">
+                    <?php echo "Identificador: " . $resultado_paquete['id_paquete'] ?>
+                </p>
+                <p class="p-2">
+                    <?php echo "Estado del Paquete: " . $resultado_paquete['estado'] ?>
+                </p>
+                <p class="p-2">
+                    <?php echo "Lote: " . $resultado_paquete['id_lote'] ?>
+                </p>
+                <p class="p-2">
+                    <?php echo "Camion: " . $resultado_paquete['matricula'] ?>
+                </p>
+                <p class="p-2">
+                    <?php echo "Camionero: " . $resultado_paquete['nombre'] ?>
+                </p>
+
+                <?php unset($_SESSION['resultado_paquete']);
+
+            } else {
+                echo "<p class='error p-2' data-i18n='ayuda1'>Donde veo el identificador de paquete?</p>
+            <p class='error p-2' data-i18n='ayuda2'>Los identificadores suelen encontrarse en la esquina inferior de las etiquetas de cada paquete</p>";
+            } ?>
+
         </div>
     </section>
 
@@ -121,10 +141,10 @@
                     <p data-i18n="customerSatisfaction">La conformidad de nuestros clientes a la hora de finalizar un
                         recorrido es lo que nos caracteriza.</p>
                     <div class="mt-4">
-                        <a type="button" class="btn btn-floating btn-warning btn-lg" href="https://facebook.com"><i
+                        <a type="button" class="btn btn-floating btn-warning btn-lg"><i
                                 class="fab fa-facebook-f"></i></a>
-                        <a type="button" class="btn btn-floating btn-warning btn-lg" href="https://twitter.com"><i class="fab fa-twitter"></i></a>
-                        <a type="button" class="btn btn-floating btn-warning btn-lg" href="https://www.google.com/intl/es/gmail/about"><i
+                        <a type="button" class="btn btn-floating btn-warning btn-lg"><i class="fab fa-twitter"></i></a>
+                        <a type="button" class="btn btn-floating btn-warning btn-lg"><i
                                 class="fab fa-google-plus-g"></i></a>
                     </div>
                 </div>
@@ -140,16 +160,16 @@
                     </div>
                     <ul class="fa-ul" style="margin-left: 1.65em;">
                         <li class="mb-3">
-                            <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2"
-                                data-i18n="homeAddress">Montevideo, Av.Gral Rivera, 11600, UY</span>
+                            <span class="fa-li"><i class="fas fa-home"></i></span><span class="ms-2">Montevideo, Av.Gral
+                                Rivera, 11600, UY</span>
                         </li>
                         <li class="mb-3">
                             <span class="fa-li"><i class="fas fa-envelope"></i></span><span
-                                class="ms-2" data-i18n="emailAddress">Abeto@gmail.com</span>
+                                class="ms-2">Abeto@gmail.com</span>
                         </li>
                         <li class="mb-3">
-                            <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2"
-                                data-i18n="phoneNumber">+594 092 951 240</span>
+                            <span class="fa-li"><i class="fas fa-phone"></i></span><span class="ms-2">+594 092 951
+                                240</span>
                         </li>
                     </ul>
                 </div>
@@ -176,68 +196,62 @@
             </div>
         </div>
 
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);" data-i18n="rightsReserved">© 2023
-            Derechos de autor
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);" data-i18n="copyright">© 2023 Derechos
+            de autor
         </div>
     </footer>
 
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
         integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc"
         crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha384-8C+nLr+VwS9+IF3TTDLZomF6uIQvofBDG3Ct7xn1fX2KU9ltHbqIYoRuHQZa4u9p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-rhcZF4Hl1mMj5FnPbX4R2R++fchYV7YB80eYnU8n4cVYx9NLsw8l5+/NEA1Jlw7d"
         crossorigin="anonymous"></script>
 
     <script>
         var textStrings = {
             es: {
-                loginTitle: "Iniciar sesión",
-                loginSubtitle: "Por favor ingrese sus credenciales",
+                quickCarry: "Quick Carry",
+                reliableLogistics: "La empresa de logística más confiable del Uruguay",
+                meshCap: "MeshCap",
+                changeLanguage: "Change language",
+                trackPackage: "Rastrear Paquete",
+                contact: "Contacto",
+                enterIdentifier: "Ingrese el identificador:",
                 aboutCompany: "Acerca de la empresa",
                 logisticsFocus: "Somos una empresa enfocada en el ámbito de la logística y nos comprometemos a brindar el mejor servicio posible.",
                 customerSatisfaction: "La conformidad de nuestros clientes a la hora de finalizar un recorrido es lo que nos caracteriza.",
-                contact: "Contacto",
+                contactUs: "CONTÁCTANOS",
                 name: "Nombre",
                 email: "Correo",
                 message: "Mensaje",
                 send: "Enviar",
-                changeLanguage: "Change language",
-                trackPackage: "Rastrear Paquete",
-                quickCarry: "Quick Carry",
-                contactUs: "Contáctanos",
-                facebook: "https://facebook.com",
-                twitter: "https://twitter.com",
-                google: "https://www.google.com/intl/es/gmail/about",
-                homeAddress: "Montevideo, Av.Gral Rivera, 11600, UY",
-                emailAddress: "Abeto@gmail.com",
-                phoneNumber: "+594 092 951 240",
-                rightsReserved: "© 2023 Derechos de autor"
+                ayuda1: "Donde veo el identificador de paquete?",
+                ayuda2: "Los identificadores suelen encontrarse en la esquina inferior de las etiquetas de cada paquete",
+                copyright: "© Derechos de autor QuickCarry.com"
             },
             en: {
-                loginTitle: "Login",
-                loginSubtitle: "Please enter your credentials",
-                aboutCompany: "About the company",
-                logisticsFocus: "We are a company focused on the field of logistics and committed to providing the best possible service.",
-                customerSatisfaction: "Customer satisfaction at the end of a journey is what characterizes us.",
+                quickCarry: "Quick Carry",
+                reliableLogistics: "Uruguay's most reliable logistics company",
+                meshCap: "MeshCap",
+                changeLanguage: "Cambiar idioma",
+                trackPackage: "Track Package",
                 contact: "Contact",
+                enterIdentifier: "Enter Identifier:",
+                aboutCompany: "About the company",
+                logisticsFocus: "We are a company focused on the logistics sector and we are committed to providing the best possible service.",
+                customerSatisfaction: "Customer satisfaction at the end of a journey is what characterizes us.",
+                contactUs: "CONTACT US",
                 name: "Name",
                 email: "Email",
                 message: "Message",
                 send: "Send",
-                changeLanguage: "Cambiar idioma",
-                trackPackage: "Track Package",
-                quickCarry: "Quick Carry",
-                contactUs: "Contact Us",
-                facebook: "https://facebook.com",
-                twitter: "https://twitter.com",
-                google: "https://www.google.com/intl/es/gmail/about",
-                homeAddress: "Montevideo, Av.Gral Rivera, 11600, UY",
-                emailAddress: "Abeto@gmail.com",
-                phoneNumber: "+594 092 951 240",
-                rightsReserved: "© 2023 All rights reserved"
+                ayuda1: "Where is the package identifier?",
+                ayuda2: "The id's often are in the bottom left corner of the package's sticker",
+                copyright: "© Copyright QuickCarry.com"
             }
         };
 
